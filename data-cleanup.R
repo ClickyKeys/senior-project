e1069482709f3223f81b846f548292cf98ba401e
Ooblet <lkn56999@gmail.com>
2021-09-06 00:15:58 -0500

    Successfully read in datasets and created a list of dataframes separated by 'already transposed' and 'not transposed'

--- data-cleanup.R
+++ data-cleanup.R
@@ -8,13 +8,6 @@ library(dplyr)
 library(ggplot2)
 library(rlist)
 
-# bind datasets (use later)
-# path <- setwd("C:/Users/lkn56/Desktop/School Stuff/Fall 2021/CSCI 487/senior-project/hospitals_current_data/EVALUATED")
-# View(path)
-# Hospital_Data <-  dir(path)
-# data_frame <- do.call(rbind.fill, lapply(Hospital_Data, read.csv))
-# View(data_frame)
-
 #import datasets - starting with first one as a test
 #make lists for the different data conditions
 Hospital_Data <- list.files(path = "C:/Users/lkn56/Desktop/School Stuff/Fall 2021/CSCI 487/hospitals_current_data/EVALUATED", pattern = "*.csv", full.names = TRUE)
@@ -24,7 +17,6 @@ Duplicates_Exist <- NULL
 
 for(j in 1:length(Hospital_Data)) {
   data = read.csv(Hospital_Data[j])
-  #View(data)
   
   #group the data by facility.ID to check for duplicate rows
   data_summary <-
@@ -32,7 +24,7 @@ for(j in 1:length(Hospital_Data)) {
     group_by(Facility.ID) %>%
     dplyr::summarize(count = n())
   
-  #check how many duplicates there are in each dataframe
+  #check how many duplicate facility ID's there are in each dataframe
   var_count <- 0
   for(i in 1:nrow(data_summary)) {
     if( data_summary$count[i] > 1 ) {
@@ -41,20 +33,36 @@ for(j in 1:length(Hospital_Data)) {
   }
   print(paste0("duplicates: ", var_count, " in dataset: ", Hospital_Data[j]))
   
+  #separate dataframes with duplicates and without duplicates into separate lists
   if( var_count == 0 ) {
     Transposed_Data <- list.append(Transposed_Data, Hospital_Data[j])
   }
   
-  if( var_count > 100 ) {
+  if( var_count > 0 ) {
     Not_Transposed <- list.append(Not_Transposed, Hospital_Data[j])
-  }
-
-  if( var_count > 0 & var_count < 100 ) {
-    Duplicates_Exist <- list.append(Duplicates_Exist, Hospital_Data[j])
   }
 }
 
-print(length(Transposed_Data))
-print(length(Not_Transposed))
-print(length(Duplicates_Exist))
-#change...
+#Not_Transposed[1] is transposed but has duplicates - Move element 1 to Transposed_Data list
+Transposed_Data <- list.append(Transposed_Data, Not_Transposed[1])
+Not_Transposed <- Not_Transposed[-1]
+
+#Create lists of dataframes for transposed and not transposed data
+NT_DF <- list()
+T_DF <- list()
+
+for(j in 1:length(Transposed_Data)) {
+  T_DF <- list.append(T_DF, read.csv(Transposed_Data[j]))
+}
+
+for(j in 1:length(Not_Transposed)) {
+  NT_DF <- list.append(NT_DF, read.csv(Not_Transposed[j]))
+}
+
+#view datasets to determine which columns are needed
+for(j in 1:length(NT_DF)) {
+  glimpse(NT_DF[j])
+  print("Next Dataframe...")
+}
+
+#Pull columns needed from 
