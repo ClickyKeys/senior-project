996e1e8406bc59315dedc7184121f52a2a2abe0b
Ooblet <lkn56999@gmail.com>
2021-09-15 16:59:06 -0500

    Fixed some stuff

--- data-cleanup.R
+++ data-cleanup.R
@@ -1,29 +1,30 @@
 #sources:
 #https://swcarpentry.github.io/r-novice-inflammation/03-loops-R/
 
-#importing and cleaning data init
-library(tidyr)
-library(tidyverse)
-library(dplyr)
-library(ggplot2)
-library(rlist)
+#importing and cleaning data
+pacman::p_load(pacman, tidyr, tidyverse, dplyr, rlist, skimr, ggplot2)
 
 #import datasets - starting with first one as a test
 #make lists for the different data conditions
 Hospital_Data <- list.files(path = "C:/Users/lkn56/Desktop/School Stuff/Fall 2021/CSCI 487/hospitals_current_data/EVALUATED", pattern = "*.csv", full.names = TRUE)
+names(Hospital_Data) <- list.files(path = "C:/Users/lkn56/Desktop/School Stuff/Fall 2021/CSCI 487/hospitals_current_data/EVALUATED", pattern = "*.csv", full.names = FALSE)
+
 Transposed_Data <- NULL
 Not_Transposed <- NULL
 Duplicates_Exist <- NULL
+T_Names <- NULL
+NT_Names <- NULL
 
 for(j in 1:length(Hospital_Data)) {
-  data = read.csv(Hospital_Data[j])
+  data = read_csv(Hospital_Data[1])
+  View(data)
   
   #group the data by facility.ID to check for duplicate rows
   data_summary <-
     data %>%
     group_by(Facility.ID) %>%
     dplyr::summarize(count = n())
-  
+
   #check how many duplicate facility ID's there are in each dataframe
   var_count <- 0
   for(i in 1:nrow(data_summary)) {
@@ -31,15 +32,18 @@ for(j in 1:length(Hospital_Data)) {
       var_count = var_count + 1;
     }
   }
-  print(paste0("duplicates: ", var_count, " in dataset: ", Hospital_Data[j]))
-  
+
+  #print(paste0("duplicates: ", var_count, " in dataset: ", Hospital_Data[j]))
   #separate dataframes with duplicates and without duplicates into separate lists
+  
   if( var_count == 0 ) {
-    Transposed_Data <- list.append(Transposed_Data, Hospital_Data[j])
+    Transposed_Data <- list.append(Transposed_Data, list(data))
+    T_Names <- list.append(T_Names, names(Hospital_Data[1]))
   }
   
   if( var_count > 0 ) {
-    Not_Transposed <- list.append(Not_Transposed, Hospital_Data[j])
+    Not_Transposed <- list.append(Not_Transposed, list(data))
+    NT_Names <- list.append(NT_Names, names(Hospital_Data[1]))
   }
 }
 
@@ -47,22 +51,24 @@ for(j in 1:length(Hospital_Data)) {
 Transposed_Data <- list.append(Transposed_Data, Not_Transposed[1])
 Not_Transposed <- Not_Transposed[-1]
 
-#Create lists of dataframes for transposed and not transposed data
-NT_DF <- list()
-T_DF <- list()
+View(Not_Transposed)
+View(Not_Transposed[[1]])
 
-for(j in 1:length(Transposed_Data)) {
-  T_DF <- list.append(T_DF, read.csv(Transposed_Data[j]))
-}
-
+#Not Transposed data frames
 for(j in 1:length(Not_Transposed)) {
-  NT_DF <- list.append(NT_DF, read.csv(Not_Transposed[j]))
+  glimpse(Not_Transposed[[j]])
+  cat("\n", NT_Names[j], "\n")
 }
 
-#view datasets to determine which columns are needed
-for(j in 1:length(NT_DF)) {
-  glimpse(NT_DF[j])
-  print("Next Dataframe...")
+#Transposed data frames
+for(j in 1:length(Transposed_Data)) {
+  glimpse(Transposed_Data[[j]])
+  cat("\nNext Dataframe...\n")
 }
 
-#Pull columns needed from 
+T_Names
+print("\nHospital General Information\n")
+glimpse(Transposed_Data[[4]])
+
+print("\nHVBP Safety\n")
+glimpse(Transposed_Data[[9]])
